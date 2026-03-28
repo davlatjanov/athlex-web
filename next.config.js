4f0bdc75e919e1ecc4165affeb99ddd605909d7a
davlatjanov <ndavlatjonov@gmail.com>
2026-03-29 04:43:48 +0900

    fix(config): transpile lucide-react to fix barrel optimize webpack error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	transpilePackages: ['lucide-react'],
 	env: {
 		REACT_APP_API_URL: process.env.REACT_APP_API_URL,
 		REACT_APP_API_GRAPHQL_URL: process.env.REACT_APP_API_GRAPHQL_URL,
